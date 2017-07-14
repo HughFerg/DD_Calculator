cdd44a6af97aa4a79c9ac5703c2320d4110cc800
hughferguson <hughferguson25@gmail.com>
2017-07-14 11:47:35 -0400

    Added ClickCounter app

--- ClickCounter/ClickCounter/ViewController.swift
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var Counter: UILabel!
     var ct = 0
+    var masterCt = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,17 +21,30 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func AddClick(_ sender: Any) {
         ct = ct + 1
         Counter.text = String(ct)
+        
+        masterCt = masterCt + 1
+        
     }
 
     @IBAction func SubtractClick(_ sender: Any) {
         ct = ct - 1
         Counter.text = String(ct)
+        
+        masterCt = masterCt + 1
+    
+    }
+    
+    @IBAction func GoToSecretView(_ sender: Any) {
+        
+        let myVC = storyboard?.instantiateViewController(withIdentifier: "SecretView") as! SecretViewController
+        myVC.masterCountPassed = masterCt
+        navigationController?.pushViewController(myVC, animated: true)
+        
     }
 }
 
