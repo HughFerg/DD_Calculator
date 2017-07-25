470a55fb41c739b4d9b4509f02c96d63acc5de04
hughferguson <hughferguson25@gmail.com>
2017-07-25 15:10:24 -0400

    Added base player addition

--- DD Calculator/DD Calculator/ViewController.swift
+++ DD Calculator/DD Calculator/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  DD Calculator
 //
-//  Created by Hugh Ferguson on 6/27/17.
+//  Created by Hugh Ferguson on 7/25/17.
 //  Copyright © 2017 Hugh Ferguson. All rights reserved.
 //
 
@@ -10,32 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var wheel: UIButton!
+    @IBOutlet weak var playerTextField: UITextField!
+    @IBOutlet weak var playerTextLabel: UILabel!
     
-    var angle = 0.0
+    var playerList = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        if (playerList.count == 0){
+            
+            playerTextLabel.text = "No Players yet!"
+        }
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-    @IBAction func spin(_ sender: Any) {
+    
+    @IBAction func addPlayer(_ sender: Any) {
         
-        UIView.animate(withDuration: 0.3, animations:({
+        if (playerTextField.text != ""){
             
-            self.angle += (Double.pi)
+        playerList.append(playerTextField.text!)
+        
+        var playerTextLabelTemp = ""
             
-            self.wheel.transform = CGAffineTransform.init(rotationAngle: CGFloat(self.angle))
+            for a in (0...playerList.count - 1){
+                
+                playerTextLabelTemp += playerList[a]
+                
+                if (a != playerList.count - 1){
+                    playerTextLabelTemp += ", "
+                }
+                
+                playerTextField.text = ""
+            }
             
-        }))
-
+            playerTextLabel.text = playerTextLabelTemp
+        
+        }
+        
+        else {
+            let emptyPlayerAlert = UIAlertController(title: "Error!", message: "Cannot add an empty player", preferredStyle: UIAlertControllerStyle.alert)
+            
+            let closeAction = UIAlertAction(title: "Close", style: .default, handler: nil)
+            
+            emptyPlayerAlert.addAction(closeAction)
+            
+            self.present(emptyPlayerAlert, animated: true)
+        }
+        
     }
+    
+
+    @IBAction func doneAdding(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
